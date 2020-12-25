8de73d15167c0f6ca2fe8fe192c7e3318f2b06e2
Krimsit <andrejsubbotin2002@yandex.ru>
2020-12-25 15:44:54 +0300

    Fix db

--- react-chat-server/src/core/db.ts
+++ react-chat-server/src/core/db.ts
@@ -1,11 +1,20 @@
 import mongoose from "mongoose"
 
+const uri: any =
+    "mongodb+srv://krimsit:ituj55hfll@chat.urzcn.mongodb.net/krimsit?retryWrites=true&w=majority"
+
 mongoose.connect(
-    "mongodb+srv://krimsit:ituj55hfll@chat.urzcn.mongodb.net/krimsit?retryWrites=true&w=majority",
+    uri,
     {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
+    },
+    (err) => {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("DB CONNECTION")
     }
 )
